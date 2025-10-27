68f89beae6346379e818075cd89e6492ff87fe7a
binoyPeries <binoyperies98@gmail.com>
2025-10-27 18:08:56 +0530

    Add approach: set of roles assigned at different levels of the hierarchy

--- casbin/demo.go
+++ casbin/demo.go
@@ -3,59 +3,103 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/casbin/casbin/v2"
+	"github.com/casbin/casbin/v2/rbac"
 )
 
+type AccessScenario struct {
+	description string
+	subject     string
+	domain      string
+	resource    string
+	action      string
+}
+
 func main() {
-	// Create enforcer with Option 2 model and policies
-	enforcer, err := casbin.NewEnforcer("option2/model.conf", "option2/policy.csv")
+	// Create enforcer with Option 1 model and policies
+	// enforcer1, err := casbin.NewEnforcer("option1/model.conf", "option1/policy.csv")
+	// if err != nil {
+	// 	log.Fatalf("Error creating enforcer: %v", err)
+	// }
+	// scenariosSet1 := []AccessScenario{
+	// 	{ // derived from org level role
+	// 		"Alice can deploy payment components as admin",
+	// 		"alice", "component:billing", "view",
+	// 	},
+	// 	{ // derived from project level role
+	// 		"full read access to dev-team on all components in projectA",
+	// 		"teamA", "component:acme/payments/billing", "view",
+	// 	},
+	// 	// promotion check, need two checks
+	// 	{
+
+	// 		"team DevOps can promote billing component",
+	// 		"teamDevOps", "component:billing", "promote",
+	// 	},
+	// 	{
+	// 		"team DevOps can deploy to staging environment",
+	// 		"teamDevOps", "env:acme/staging", "deploy_to",
+	// 	},
+	// }
+	// // Requirement 1: Check whether a user U can perform action X on resource Y
+	// testAccessChecks(enforcer1, scenariosSet1)
+
+	// Create enforcer with Option 3 model and policies
+	enforcer3, err := casbin.NewEnforcer("option4/model.conf", "option4/policy.csv")
+	var DomainPrefix rbac.MatchingFunc = func(reqDom, polDom string) bool {
+		return reqDom == polDom || strings.HasPrefix(reqDom, polDom+"/")
+	}
+
+	// Register it for the "g" role system (name it anything you like).
+	enforcer3.AddNamedDomainMatchingFunc("g", "DomainPrefix", DomainPrefix)
+
 	if err != nil {
 		log.Fatalf("Error creating enforcer: %v", err)
 	}
-	// Requirement 1: Check whether a user can perform action X on resource Y
-	testAccessChecks(enforcer)
+	scenariosSet3 := []AccessScenario{
+		{ // derived from org level role
+			"TeamA can deploy billing components as proj-deployer",
+			"group:teamA", // r.sub
+			"org:acme/project:payments/component:billing", // r.dom
+			"component", // r.obj
+			"deploy",
+		},
+		{ // derived from org level role
+			"TeamB can deploy billing components as proj-deployer",
+			"group:teamB", // r.sub
+			"org:acme/project:hello/component:billing", // r.dom
+			"component", // r.obj
+			"deploy",
+		},
+		{ // derived from org level role
+			"TeamB can deploy billing components as proj-deployer",
+			"group:teamC", // r.sub
+			"org:acme/project:payments/component:hello", // r.dom
+			"component", // r.obj
+			"deploy",
+		},
+
+	}
+
+	// Requirement 1: Check whether a user U can perform action X on resource Y
+	testAccessChecks(enforcer3, scenariosSet3)
 
 }
 
 // Requirement: Check whether user can perform action X on resource Y
-func testAccessChecks(enforcer *casbin.Enforcer) {
-
-	scenarios := []struct {
-		description string
-		subject     string
-		resource    string
-		action      string
-	}{
-		{ // derived from org level role
-			"Alice can deploy payment components as admin",
-			"alice", "component:billing", "view",
-		},
-		{ // derived from project level role
-			"full read access to dev-team on all components in projectA",
-			"teamA", "component:acme/payments/billing", "view",
-		},
-		// promotion check, need two checks
-		{
-
-			"team DevOps can promote billing component",
-			"teamDevOps", "component:billing", "promote",
-		},
-		{
-			"team DevOps can deploy to staging environment",
-			"teamDevOps", "env:acme/staging", "deploy_to",
-		},
-	}
-
+func testAccessChecks(enforcer *casbin.Enforcer, scenarios []AccessScenario) {
 	for _, scenario := range scenarios {
 		result, _ := enforcer.Enforce(
 			scenario.subject,
+			scenario.domain,
 			scenario.resource,
 			scenario.action,
 		)
-		fmt.Printf("Scenario: %s || result=%v\n", scenario.description, result)
-		testGetUserPermissions(enforcer, scenario.subject)
-		fmt.Println("--------------------------------------------------")
+		fmt.Printf("Scenario: %s\n", scenario.description)
+		fmt.Printf("  Result: %v\n\n", result)
+		getUserActions(enforcer, scenario.subject, scenario.domain)
 
 	}
 }
@@ -73,4 +117,13 @@ func testGetUserPermissions(enforcer *casbin.Enforcer, user string) {
 	fmt.Printf("Roles: %v\n", roles)
 	fmt.Printf("Inherited Roles: %v\n", implicitRoles)
 
+}
+
+func getUserActions(enforcer *casbin.Enforcer, user string, domain string) {
+	roles := enforcer.GetRolesForUserInDomain(user, domain)
+	fmt.Printf("User: %s, Domain: %s, Roles: %v\n", user, domain, roles)
+	for _, role := range roles {
+		permissions, _ := enforcer.GetFilteredPolicy(0, role)
+		fmt.Printf("  Role: %s, Permissions: %v\n", role, permissions)
+	}
 }
